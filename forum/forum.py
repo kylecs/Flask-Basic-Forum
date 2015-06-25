5b4c676465fa0c0865c77e40f52634bf63d9034a
Kyle Spencer <kylecspencer@gmail.com>
2015-06-25 16:21:11 -0400

    specifying ip

--- forum/forum.py
+++ forum/forum.py
@@ -88,6 +88,6 @@ def action_createaccount():
 #RUN
 if __name__ == "__main__":
 	#db.create_all()
-	port = int(os.environ["PORT"])
-	app.run(debug=True, port=port)
+	port = int(os.environ.get("PORT", 33507))
+	app.run(host='0.0.0.0', port=port)
 #app.run(debug=True)
